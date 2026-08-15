a0441357b8fe980c84ace964aa6e05d4fc2263d4
madiyarone99-hash <madiyar.one99@gmail.com>
2026-08-16 00:45:52 +0500

    Add b64-1d chunk and multipart CDN loader

--- assets/index-BupAa7jJ.js
+++ assets/index-BupAa7jJ.js
@@ -1,5 +1,14 @@
-const a = await (await fetch(new URL("./chunks/b64-0.txt", import.meta.url))).text();
-const b = await (await fetch(new URL("./chunks/b64-1.txt", import.meta.url))).text();
-const bin = Uint8Array.from(atob(a + b), c => c.charCodeAt(0));
-const url = URL.createObjectURL(new Blob([bin], { type: "text/javascript" }));
+const parts = [
+  './chunks/b64-0.txt',
+  './chunks/b64-1a.txt',
+  './chunks/b64-1b.txt',
+  './chunks/b64-1c.txt',
+  './chunks/b64-1d.txt',
+];
+const texts = await Promise.all(parts.map((u) => fetch(u).then((r) => {
+  if (!r.ok) throw new Error('failed ' + u);
+  return r.text();
+})));
+const code = atob(texts.join(''));
+const url = URL.createObjectURL(new Blob([code], { type: 'text/javascript' }));
 await import(url);
