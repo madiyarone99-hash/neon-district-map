7d6059eca3846bbca7f9e6d4782592046578bbe8
madiyarone99-hash <madiyar.one99@gmail.com>
2026-08-16 00:48:08 +0500

    Add b64-0d and patched 8-part CDN loader

--- assets/index-BupAa7jJ.js
+++ assets/index-BupAa7jJ.js
@@ -1,5 +1,8 @@
 const parts = [
-  './chunks/b64-0.txt',
+  './chunks/b64-0a.txt',
+  './chunks/b64-0b.txt',
+  './chunks/b64-0c.txt',
+  './chunks/b64-0d.txt',
   './chunks/b64-1a.txt',
   './chunks/b64-1b.txt',
   './chunks/b64-1c.txt',
@@ -9,6 +12,8 @@ const texts = await Promise.all(parts.map((u) => fetch(u).then((r) => {
   if (!r.ok) throw new Error('failed ' + u);
   return r.text();
 })));
+// one-byte MCP transport corruption fix in b64-0c
+texts[2] = texts[2].slice(0, 776) + 'v' + texts[2].slice(777);
 const code = atob(texts.join(''));
 const url = URL.createObjectURL(new Blob([code], { type: 'text/javascript' }));
 await import(url);
